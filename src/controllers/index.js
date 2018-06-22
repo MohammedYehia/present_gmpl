5459b1a8a08a220f209185d25cb6bcc4c05f62c7
ramyalshurafa <ramyshurafa@hotmail.com>
2018-06-23 00:48:51 +0300

    admin logout Related #4

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import home from './home';
 import { getLogin, postLogin } from './login';
+import logout from './logout';
 
 const router = express.Router();
 
 router
   .get('/', home)
   .get('/admin/login', getLogin)
-  .post('/admin/login', postLogin);
+  .post('/admin/login', postLogin)
+  .get('/admin/logout', logout);
 
 export default router;
