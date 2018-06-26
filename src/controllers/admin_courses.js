10467b71c71030ae6a1def34d43e553f69bd2d9d
Ahmed Shatat <a.shatat@hotmail.com>
2018-06-26 22:40:36 +0300

    delete commented lines admin_course.js

--- src/controllers/admin_courses.js
+++ src/controllers/admin_courses.js
@@ -6,7 +6,6 @@ export default (req, res) => {
     where: { fees: { [Op.ne]: null } },
   })
     .then((result) => {
-      // res.json(result);
       res.render('admin_courses', {
         pageTitle: 'admin Courses',
         layout: 'admin',
@@ -15,23 +14,3 @@ export default (req, res) => {
       });
     });
 };
-// import models from './../database/models/index';
-//
-// const { Op } = models.Sequelize;
-// export default (req, res) => {
-//   models.Bookings.findAll({
-//     include: [{
-//       model: models.EventCourses,
-//       where: { fees: { [Op.ne]: null } },
-//     },
-//     {
-//       model: models.Rooms,
-//     }],
-//   })
-//     .then(result => res.render('admin_courses', {
-//       pageTitle: 'admin Courses',
-//       layout: 'admin',
-//       courses: result,
-//       jsFile: ['admin'],
-//     }));
-// };
