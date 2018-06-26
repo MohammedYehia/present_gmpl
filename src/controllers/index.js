f5ec000a9894d7bbf877b883144bb5bc44c1e2ff
ramyalshurafa <ramyshurafa@hotmail.com>
2018-06-26 10:02:23 +0300

    create admin courses panel files

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,6 +4,7 @@ import { getLogin, postLogin } from './login';
 import logout from './logout';
 import events from './events';
 import adminEvents from './admin_events';
+import adminCourses from './admin_courses';
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router
   .post('/admin/login', postLogin)
   .get('/admin/logout', logout)
   .get('/events', events)
-  .get('/admin/events', adminEvents);
+  .get('/admin/events', adminEvents)
+  .get('/admin/courses', adminCourses);
 export default router;
