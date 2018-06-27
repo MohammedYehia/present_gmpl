eff420a493b2e30f9f4bb2236be4eab90bfd02bb
Ahmed Shatat <a.shatat@hotmail.com>
2018-06-27 21:04:45 +0300

    change fullcalender lib to local files

--- src/controllers/bookings.js
+++ src/controllers/bookings.js
@@ -1,19 +1,20 @@
 export default (req, res) => {
   res.render('bookings', {
     jsFile: [
+      'sweetalert.min',
       'verify_form',
-      'bookings'
+      'bookings',
     ],
     jsLinks: [
-      'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/locale-all.js',
-      'https://fullcalendar.io/releases/fullcalendar-scheduler/1.9.4/scheduler.min.js'
+      'jquery.min',
+      'moment.min',
+      'fullcalendar.min',
+      'locale-all',
+      'scheduler.min',
     ],
     styleLinks: [
-      'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css',
-      'https://fullcalendar.io/releases/fullcalendar-scheduler/1.9.4/scheduler.min.css'
-    ]
-  })
+      'fullcalendar.min',
+      'scheduler.min',
+    ],
+  });
 };
