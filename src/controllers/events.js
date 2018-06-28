c43dcd83e2217e61c30e3e343438b3d6d38bc5c8
Mohammed <MohammedYehia@users.noreply.github.com>
2018-06-28 12:14:38 +0300

    Update events.js

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -2,55 +2,21 @@ import models from '../database/models';
 
 export default (req, res) => {
   const { url } = req;
-  models.EventCourses.findAll({})
-    .then((result) => {
-      const courses = [];
-      const events = [];
-      let counter = 0;
-      result.forEach((cell) => {
-        if (cell.fees !== null) {
-          counter += 1;
-          courses.push(Object.assign(
-            {},
-            {
-              id: cell.id,
-              title: cell.title,
-              image: cell.image,
-              description: cell.description,
-              fees: cell.fees,
-            },
-          ));
-        } else {
-          models.Bookings.findOne({
-            where: { event_id: cell.id },
-            include: {
-              model: models.Rooms,
-            },
-          })
-            .then((booking) => {
-              counter += 1;
-              if (booking) {
-                events.push(Object.assign(
-                  {},
-                  {
-                    id: cell.id,
-                    title: cell.title,
-                    image: cell.image,
-                    description: cell.description,
-                    event_time: booking.start_at,
-                    room_name: booking.room.room_name,
-                  },
-                ));
-              }
-              if (counter === result.length) {
-                if (url.includes('/events') && url.length === 7) {
-                  res.render('events', { events, cssFile: 'style' });
-                } else if (url.includes('/courses') && url.length === 8) {
-                  res.render('events', { courses, cssFile: 'style' });
-                }
-              }
-            });
-        }
-      });
-    });
+  models.EventCourses.findAll({}).then((data) => {
+    const courses = [];
+    const events = [];
+    for (const cell of data) {
+      if (cell.fees ) {
+        courses.push(cell);
+      } else{
+        events.push(cell);
+      }
+    }
+
+    if (url.includes('/events') && url.length === 7) {
+      res.render('events', { events, cssFile: 'style' });
+    } else if (url.includes('/courses') && url.length === 8) {
+      res.render('events', { courses, cssFile: 'style' });
+    }
+  });
 };
